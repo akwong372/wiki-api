ab03584829abe555e696d7aba990c72d1a3e825b
akwong372 <akwong372@gmail.com>
2020-07-07 16:43:33 -0700

    add get, put, patch, delete routes for single articles

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
     res.send('home page');
 });
 
+// requests for all articles
+
 app.route('/articles').get((req, res) => {
     db.ArticleModel.find((err, articles) => {
         if (err) {
@@ -50,6 +52,63 @@ app.route('/articles').get((req, res) => {
                 res.redirect('/articles');
             }
         });
+    });
+
+// requests for individual articles
+
+app.route('/articles/:article').get((req, res) => {
+    const title = req.params.article;
+    db.ArticleModel.findOne({ title }, (err, article) => {
+        if (err) {
+            console.log(`Error finding article: ${err}`);
+            res.redirect('/articles');
+        } else if (article) {
+            console.log('Article found');
+            res.json(article);
+        } else {
+            res.json('No articles with that title.');
+        }
+    });
+})
+    .put((req, res) => {
+        const title = req.params.article;
+        const newTitle = req.body.title;
+        const newContent = req.body.content;
+        db.ArticleModel.updateOne({ title }, { title: newTitle, content: newContent }, err => {
+            if (err) {
+                console.log(`Error replacing article: ${err}`);
+                res.redirect(`/articles/${title}`);
+            } else {
+                console.log(`Article ${title} replaced`);
+                res.redirect(`/articles/${newTitle}`);
+            }
+        })
+    })
+    .patch((req, res) => {
+        const title = req.params.article;
+        const patchData = req.body;
+
+        db.ArticleModel.findOneAndUpdate({ title }, { $set: patchData }, { new: true }, (err, article) => {
+            if (err) {
+                console.log(`Error updating article: ${err}`);
+                res.redirect(`/articles/${title}`);
+            } else {
+                console.log(`Article ${title} updated ${JSON.stringify(article)}`);
+                res.redirect(`/articles/${article.title}`);
+            }
+        })
+    })
+    .delete((req, res) => {
+        const title = req.params.article;
+        db.ArticleModel.deleteOne({ title }, err => {
+            if (err) {
+                console.log(`Error deleting article: ${err}`);
+                res.redirect(`/articles/${title}`);
+            } else {
+                console.log(`Deleted article: ${title}`);
+                res.redirect('/articles');
+            }
+        })
     });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
