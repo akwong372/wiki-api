e76c88c8853ab801516c67209f22e95b565affdc
akwong372 <akwong372@gmail.com>
2020-07-07 14:52:03 -0700

    add get, post, and delete routes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/wikiDB', { useUnifiedTopology: true, useNewUrlParser: true });
 
 const articleSchema = new mongoose.Schema({
     title: String,
--- index.js
+++ index.js
@@ -9,7 +9,47 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send('ok');
+    res.send('home page');
 });
+
+app.route('/articles').get((req, res) => {
+    db.ArticleModel.find((err, articles) => {
+        if (err) {
+            console.log(`Failed to get articles: ${err}`);
+            res.send('error getting');
+        } else {
+            console.log('Found all articles')
+            res.json(articles);
+        }
+    });
+})
+    .post((req, res) => {
+        const title = req.body.title;
+        const content = req.body.content;
+        const newArticle = new db.ArticleModel({
+            title,
+            content
+        });
+        newArticle.save((err) => {
+            if (err) {
+                console.log(`Error saving new article: ${err}`);
+                res.redirect('/articles');
+            } else {
+                console.log(`Created article: ${title}`)
+                res.redirect('/articles');
+            }
+        });
+    })
+    .delete((req, res) => {
+        db.ArticleModel.deleteMany(err => {
+            if (err) {
+                console.log(`Error deleting articles: ${err}`);
+                res.redirect('/articles');
+            } else {
+                console.log('Deleted all articles')
+                res.redirect('/articles');
+            }
+        });
+    });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
